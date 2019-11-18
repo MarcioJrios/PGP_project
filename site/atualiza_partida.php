dd9a77742026860fb6248480a58ecb5c4a591024
Philipe Soares <31110942+godmerlin@users.noreply.github.com>
2019-11-18 15:39:51 -0300

    Add files via upload

--- site/atualiza_partida.php
+++ site/atualiza_partida.php
@@ -16,12 +16,47 @@ if(isset($_SESSION['tipo_usuario'])){
 	<div class="Partidas">
 	<form action="atualiza_partida.php" method="post" id="form-contato">
 	<?php 
-        $sql = "select id_partida,id_equipe1,id_equipe2 from partidas WHERE pontos_equipe1=NULL and pontos_equipe2=NULL";
+        $sql = "select id_partida,id_equipe1,id_equipe2 from partidas WHERE pontos_equipe1 is NULL and pontos_equipe2 is NULL";
         $res = mysqli_query($conexao, $sql);
-        $partida = mysqli_fetch_array($res);
-        
-	while ($res = $assoc->fetch_assoc()){
-		echo '<p><input type="number" name="equipe1" value='.$res['id_game'].'> '.$res['nome'].'</p>';
+	while ($lista = mysqli_fetch_array($res)){
+		
+		//  echo '<p><input type="number" name="equipe1" value='.$lista['id_partida'].'> '.$lista['nome'].'</p>';
+		#equipe 1
+		$equipe1 = $lista['id_equipe1'];
+		$sql = " select * from equipes WHERE id_equipe = $equipe1";
+		$equipe1 = mysqli_query($conexao, $sql);
+		$equipe1 = mysqli_fetch_array($equipe1);
+		?>
+			<div class="equipe1">
+				
+					<figure>
+						<figcaption>
+						<?=$equipe1['sigla'];?>
+						</figcaption>
+					</figure>
+				</a>
+			</div>
+		
+		<h3>VS</h3>
+		<?php
+
+		#equipe 2
+		$equipe2 = $lista['id_equipe2'];
+		$sql = " select * from equipes WHERE id_equipe = $equipe2";
+		$equipe2 = mysqli_query($conexao, $sql);
+		$equipe2 = mysqli_fetch_array($equipe2);
+		?>
+			<div class="equipe2">
+			
+					<figure>
+						<figcaption>
+						<?=$equipe2['sigla'];?>
+						</figcaption>
+					</figure>
+				</a>
+			</div>
+		</div>  <!--Div de partida-->
+	<?php	
 	}
 	?>
 	<div class="botao">
