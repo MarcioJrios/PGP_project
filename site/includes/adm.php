8a46333022d7dc6448995764e8ec3c485c72da74
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-18 17:39:35 -0300

    Add files via upload

---
+++ site/includes/adm.php
@@ -0,0 +1,17 @@
+<div class='menu_adm'>
+	<div class='item'>
+	<a href="cadastro_equipe.php">Cadastrar Equipe</a>
+	</div>
+	<div class='item'>
+	<a href="cadastro_campeonato.php">Cadastrar Campeonato</a>
+	</div>
+	<div class='item'>
+	<a href="cadastro_partida.php">Cadastrar Partidas</a>
+	</div>
+	<div class='item'>
+	<a href="cadastro_game.php">Cadastrar Games</a>
+	</div>
+	<div class='item'>
+	<a href="atualiza_partida.php">Atualiza Resultados</a>
+	</div>
+</div>
