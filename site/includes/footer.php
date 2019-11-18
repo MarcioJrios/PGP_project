0a301d541e375674d9a5b0f3354e5276304c73f6
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-18 15:02:44 -0300

    Add files via upload

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -1,9 +1,13 @@
-	<hr>
+	<div class="divisao-header-class">
+	</div>
+
 	<footer>
-	<p>PilaBet - Chapecó/SC © 2019 | Todos os direitos reservados.</p>
-	<p>O site PilaBet é operado pela Turma do 6°semestre de Ciencia da Computação da UFFS.
-	O acesso a sites de jogos online com apostas em é proibido em alguns países.
-	É responsabilidade exclusiva de cada jogador cumprir a lei ao acessar o PilaBet.</p>
+	<div class='footer'>
+	<p>PilaBet - Chapecó/SC © 2019 | Todos os direitos reservados.<br>
+	O site PilaBet é operado pela Turma do 6° semestre de Ciencia da Computação da UFFS.<br>
+		O acesso a sites de jogos online com apostas em é proibido em alguns países.<br>
+		É responsabilidade exclusiva de cada jogador cumprir a lei ao acessar o PilaBet.</p>
+	</div>
 	</footer>
 </body>
 </html>
