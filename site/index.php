0e9c107825e9c228ca7a2abbc6baece1da56a6a4
Marcio J. Rios <31110984+MarcioJrios@users.noreply.github.com>
2019-11-25 17:35:03 -0300

    Started CSS

--- site/index.php
+++ site/index.php
@@ -1,6 +1,22 @@
 <?php
 
 include "includes/header.php";
-include "includes/footer.php";
+?>
+<link href="css/index.css" rel="stylesheet" type="text/css">
 
+<div class = "sections">
+<?php
+include "includes/lista_esports.php";
+include "includes/lista_camp.php";
+include "includes/lista_partidas.php";
+?>
+</div>
+<div class = "asides">
+<?php
+include "includes/partidas_finalizadas.php";
+include "includes/mais_apostados.php";
+?>
+</div>
+<?php
+include "includes/footer.php";
 ?>
