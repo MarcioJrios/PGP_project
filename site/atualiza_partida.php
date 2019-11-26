38149f1eac3a4513fc7cb739e1313e2576a4497e
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-25 23:51:01 -0300

    Add files via upload

--- site/atualiza_partida.php
+++ site/atualiza_partida.php
@@ -11,45 +11,77 @@ if(isset($_SESSION['tipo_usuario'])){
 	header("Location: login.php");
 }
 
-$id_partida = isset($_GET['partida']) ? $_GET['partida'] : '';
+@$id_partida = isset($_GET['partida']) ? $_GET['partida'] : '';
 $erros = array();
 
-if(isset($_POST['cadastrar'])){
-	$equipe1 = $_POST['equipe1'];
-	$equipe2 = $_POST['equipe2'];
-	$partida = $_POST['id_partida'];
-	$id_partida = $partida;
-	$email = $_SESSION['email'];
+$sttm2 = $conexao->prepare("select id_partida,id_equipe1,id_equipe2 from partidas WHERE id_partida = ?;");
+$sttm2->bind_param("i", $id_partida);
+$sttm2->execute();
+$res = $sttm2->get_result();
+$res = $res->fetch_assoc();
 
-	$sttm = $conexao->prepare("UPDATE partidas SET pontos_equipe1 = ?, pontos_equipe2 = ? where id_partida = ? ;");
-	$sttm->bind_param("iii", $equipe1, $equipe2,$partida);
+#equipe 1
+$equipe1_id = $res['id_equipe1'];
+$sttm = $conexao->prepare("select * from equipes WHERE id_equipe = ?");
+$sttm->bind_param("i", $equipe1_id);
+$sttm->execute();
+$equipe1 = $sttm->get_result();
+$equipe1 = $equipe1->fetch_assoc();
+
+#equipe 2
+$equipe2_id = $res['id_equipe2'];
+$sttm = $conexao->prepare("select * from equipes WHERE id_equipe = ?");
+$sttm->bind_param("i", $equipe2_id);
+$sttm->execute();
+$equipe2 = $sttm->get_result();
+$equipe2 = $equipe2->fetch_assoc();
+
+if(isset($_POST['cadastrar'])){
+	$pontos_equipe1 = $_POST['pontos_equipe1'];
+	$pontos_equipe2 = $_POST['pontos_equipe2'];
+	$partida = $_POST['id_partida'];
+
+	$sttm = $conexao->prepare("UPDATE partidas SET pontos_equipe1 = ?, pontos_equipe2 = ? where id_partida = ?");
+	$sttm->bind_param("iii", $pontos_equipe1, $pontos_equipe2,$partida);
 	$sttm->execute();
 
-	$statement = $conexao->prepare("select valor from apostas WHERE id_partida = ? and email = ?;");
-	$statement->bind_param("is", $partida, $email);
-	$statement->execute();
-	$res1 = $statement->get_result();
-	$res1 = $res->fetch_assoc();
-	$aposta_user = $res1['valor'];
-
-	$soma = 0;
-
-	$sttm2 = $conexao->prepare("select * from apostas WHERE id_partida = $partida;");
-	$sttm2->execute();
-	$res2 = $sttm2->get_result();
-
-	while($resultado = $res2->fetch_assoc())
-		$soma = $soma + $resultado['valor'];
-	
-	print_r($soma);
-
-	if($equipe1 == $equipe2){
-		$saldo = $aposta_user + $_SESSION['saldo'];
-		$statement = $conexao->prepare("UPDATE usuarios SET saldo = ? where email = ?;");
-		$statement->bind_param("ds", $saldo, $_SESSION['email']);
-		$statement->execute();
+	if($pontos_equipe1 > $pontos_equipe2){
+		$id_ganhador = $equipe1_id;
+		$id_perdedor = $equipe2_id;
+	}else{
+		$id_perdedor = $equipe1_id;
+		$id_ganhador = $equipe2_id;
 	}
-	else{
+
+	$sttm = $conexao->prepare("select sum(valor) as valor from apostas WHERE id_partida = $partida");
+	$sttm->execute();
+	$soma_total = $sttm->get_result()->fetch_assoc()['valor'];
+
+	$sttm = $conexao->prepare("select sum(valor) as valor from apostas WHERE id_partida = ? and id_equipe = ?");
+	$sttm->bind_param("ii", $partida, $id_perdedor);
+	$sttm->execute();
+	$soma_perdedor = $sttm->get_result()->fetch_assoc()['valor'];
+
+
+	$sttm = $conexao->prepare("select * from apostas WHERE id_partida = ? and id_equipe = ?");
+	$sttm->bind_param("ii", $partida, $id_ganhador);
+	$sttm->execute();
+	$res2 = $sttm->get_result();
+	
+	while($resultado = $res2->fetch_assoc()){
+		$sttm = $conexao->prepare("select saldo from usuarios WHERE email = ?");
+		$sttm->bind_param("s", $resultado['email']);
+		$sttm->execute();
+		$resultado_usuarios = $sttm->get_result()->fetch_assoc();
+
+		if(empty($soma_perdedor))
+			$saldo = $resultado['valor'];
+			
+		$saldo = $saldo + $resultado_usuarios['saldo'];
+
+		$sttm = $conexao->prepare("UPDATE usuarios SET saldo = ? WHERE email = ?");
+		$sttm->bind_param("ds", $saldo, $resultado['email']);
+		$sttm->execute();
 
 	}
 }
@@ -62,7 +94,7 @@ if(isset($_POST['cadastrar'])){
 		if(!(isset($_POST['cadastrar'])) || (isset($erros) && count($erros))){
 		?>
 
-		<form action="atualiza_partida.php" method="POST" id="form-contato">
+		<form action="atualiza_partida.php?partida=<?=$id_partida?>" method="POST" id="form-contato">
 			<div class="Partidas">
 				<?php
 					if(isset($erros['insert'])){
@@ -70,44 +102,23 @@ if(isset($_POST['cadastrar'])){
 					}
 				?>
 			<h2>Jogos Disponíveis</h2>
-			<?php 
-				$sql = "select id_partida,id_equipe1,id_equipe2 from partidas WHERE id_partida = $id_partida";
-				$res = mysqli_query($conexao, $sql);
-				$res = mysqli_fetch_array($res);
-			
-				#equipe 1
-				$equipe1 = $res['id_equipe1'];
-				$sql = " select * from equipes WHERE id_equipe = $equipe1";
-				$equipe1 = mysqli_query($conexao, $sql);
-				$equipe1 = mysqli_fetch_array($equipe1);
-				?>
 				<div class="form-item">
-					<label for="equipe1" class="label-alinhado">Resultado da equipe 1 (<?=$equipe1['sigla']?>):</label>
-					<?='<input type="number" min="0" name="equipe1" value='.$res['id_partida'].'>';?>
-					<span class="msg-erro" id="msg-equipe1"><?=@$erros['equipe1'];?></span>
+					<label for="pontos_equipe1" class="label-alinhado">Resultado da equipe 1 (<?=$equipe1['sigla']?>):</label>
+					<input type="number" min="0" name="pontos_equipe1" value='<?=isset($pontos_equipe1) ? $pontos_equipe1 : 0;?>'>
 				</div>
 				<h3>VS</h3>
-
-				<?php
-				#equipe 2
-				$equipe2 = $res['id_equipe2'];
-				$sql = " select * from equipes WHERE id_equipe = $equipe2";
-				$equipe2 = mysqli_query($conexao, $sql);
-				$equipe2 = mysqli_fetch_array($equipe2);
-				?>
+				
 				<div class="form-item">
-					<label for="equipe2" class="label-alinhado">Resultado da equipe 2 (<?=$equipe2['sigla']?>):</label>
-					<?='<input type="number" min="0" name="equipe2" value='.$res['id_partida'].'>';?>
-					<span class="msg-erro" id="msg-equipe2"><?=@$erros['equipe2'];?></span>
+					<label for="pontos_equipe2" class="label-alinhado">Resultado da equipe 2 (<?=$equipe2['sigla']?>):</label>
+					<input type="number" min="0" name="pontos_equipe2" value='<?=isset($pontos_equipe2) ? $pontos_equipe2 : 0;?>'>
 				</div>
 
-				<?php	
-				?>
 				<input type="hidden" id="id_partida" name="id_partida" value="<?=isset($id_partida) ? $id_partida : '';?>">
+				<br>
 				<div class="botao">
 					<div class="form-item">
 						<label class="label-alinhado"></label>
-						<input type="submit" id="botao" value="Cadastrar">
+						<input type="submit" id="botao" value="Cadastrar" name="cadastrar">
 					</div>
 				</div>
 				<div class="botao1">
@@ -121,7 +132,7 @@ if(isset($_POST['cadastrar'])){
 
 		<?php
 		}else{
-			echo "<p>Partida <strong>atualizada</strong> com sucesso! <a href='adm.php'>Clique para voltar a tela de administrador</a></p>";
+			echo '<p>Partida <strong>atualizada</strong> com sucesso! <a href="adm.php">Clique para voltar a tela de administrador</a></p>';
 		}
 		?>
 	</div>
