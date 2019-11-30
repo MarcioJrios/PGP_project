b716326d7687facd54969d73eba21b6646e31276
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-30 20:11:25 -0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -2,21 +2,23 @@
 
 include "includes/header.php";
 ?>
-<link href="css/index.css" rel="stylesheet" type="text/css">
 
-<div class = "sections">
-<?php
-include "includes/lista_esports.php";
-include "includes/lista_camp.php";
-include "includes/lista_partidas.php";
-?>
+<div class="index">
+    <div class = "sections">
+    <?php
+    include "includes/lista_esports.php";
+    include "includes/lista_camp.php";
+    include "includes/lista_partidas.php";
+    ?>
+    </div>
+    <div class = "asides">
+    <?php
+    include "includes/partidas_finalizadas.php";
+    include "includes/mais_apostados.php";
+    ?>
+    </div>
 </div>
-<div class = "asides">
-<?php
-include "includes/partidas_finalizadas.php";
-include "includes/mais_apostados.php";
-?>
-</div>
+
 <?php
 include "includes/footer.php";
 ?>
