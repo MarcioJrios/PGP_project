0f10f838dcc416f2581740e79e118f92d5c2bb7e
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-30 20:02:50 -0300

    Update mais_apostados.php

--- site/includes/mais_apostados.php
+++ site/includes/mais_apostados.php
@@ -8,59 +8,40 @@
 	$statement->execute();
 	$assoc = $statement->get_result();
 	while ($resultado = $assoc->fetch_assoc()){
-        ?>
-        <div class="partida">
-        <?php
+	?>
+		<div class="partida">
+
+		<h3>Mais Apostados</h3>
+		<?php
 		$partida = $resultado['id_partida'];
 		$sql = " select * from partidas WHERE id_partida = $partida";
 		$res = mysqli_query($conexao, $sql);
-        $partida = mysqli_fetch_array($res);
+		$partida = mysqli_fetch_array($res);
 
-        $camp = $partida['id_camp'];
-        $sql = " select sigla from campeonatos WHERE id_camp = $camp";
-		$camp = mysqli_query($conexao, $sql);
-        $camp = mysqli_fetch_array($res);
-        ?>
-        <h4><?=$camp['sigla'];?></h4>
-        <?php
-        
-        
-        #equipe 1
-        $equipe1 = $partida['id_equipe1'];
-        $sql = " select * from equipes WHERE id_equipe = $equipe1";
-        $equipe1 = mysqli_query($conexao, $sql);
-        $equipe1 = mysqli_fetch_array($equipe1);
+		$camp = $partida['id_camp'];
+		$sql = " select sigla from campeonatos WHERE id_camp = $camp";
+			$camp = mysqli_query($conexao, $sql);
+		$camp = mysqli_fetch_array($res);
 		?>
-			<div class="equipe1">
-					<figure>
-						<figcaption>
-                        <?=$equipe1['sigla'];?>
-						</figcaption>
-					</figure>
-				</a>
-			</div>
-        
-        <h3>VS</h3>
-        <?php
+		<h4><?=$camp['sigla'];?></h4>
+		<?php
 
-        #equipe 2
-        $equipe2 = $partida['id_equipe2'];
-        $sql = " select * from equipes WHERE id_equipe = $equipe2";
-        $equipe2 = mysqli_query($conexao, $sql);
-        $equipe2 = mysqli_fetch_array($equipe2);
-		?>
-			<div class="equipe2">
-					<figure>
-						<figcaption>
-                        <?=$equipe2['sigla'];?>
-						</figcaption>
-					</figure>
-				</a>
-			</div>
-        <?=$partida['horario_termino'];?>
+
+		#equipe 1
+		$equipe1 = $partida['id_equipe1'];
+		$sql = " select * from equipes WHERE id_equipe = $equipe1";
+		$equipe1 = mysqli_query($conexao, $sql);
+		$equipe1 = mysqli_fetch_array($equipe1);
+		#equipe 2
+		$equipe2 = $partida['id_equipe2'];
+		$sql = " select * from equipes WHERE id_equipe = $equipe2";
+		$equipe2 = mysqli_query($conexao, $sql);
+		$equipe2 = mysqli_fetch_array($equipe2);
+		
+		echo ''.$equipe1['sigla'].' VS '.$equipe2['sigla'].'<p> '.$partida['horario_termino'].'</p>';?>
         </div>  <!--Div de partida-->
-<?php
-}
-?>
+	<?php
+	}
+	?>
 	</div>
 </aside>
