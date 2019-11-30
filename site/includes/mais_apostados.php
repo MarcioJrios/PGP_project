ed832611f0cf7d8d4fdf7d4f20fe766c8388311e
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-11-30 20:56:58 -0300

    Update mais_apostados.php

--- site/includes/mais_apostados.php
+++ site/includes/mais_apostados.php
@@ -1,47 +1,50 @@
 <aside class="mais_apostados">
 	<!-- container de mais pedidos -->
 	<div class="lista-partidas">
-	<?php
-	include_once "conexao.php";
-
-	$statement = $conexao->prepare("select distinct apostas.id_partida, count(*) as ocorrencias from apostas join partidas on apostas.id_partida = partidas.id_partida group by id_partida order by ocorrencias desc limit 10");
-	$statement->execute();
-	$assoc = $statement->get_result();
-	while ($resultado = $assoc->fetch_assoc()){
-	?>
 		<div class="partida">
 
 		<h3>Mais Apostados</h3>
 		<?php
-		$partida = $resultado['id_partida'];
-		$sql = " select * from partidas WHERE id_partida = $partida";
-		$res = mysqli_query($conexao, $sql);
-		$partida = mysqli_fetch_array($res);
+		include_once "conexao.php";
 
-		$camp = $partida['id_camp'];
-		$sql = " select sigla from campeonatos WHERE id_camp = $camp";
-			$camp = mysqli_query($conexao, $sql);
-		$camp = mysqli_fetch_array($res);
-		?>
-		<h4><?=$camp['sigla'];?></h4>
+		$statement = $conexao->prepare("select distinct apostas.id_partida, count(*) as ocorrencias from apostas join partidas on apostas.id_partida = partidas.id_partida group by id_partida order by ocorrencias desc limit 10");
+		$statement->execute();
+		$assoc = $statement->get_result();
+		if(!$assoc->fetch_assoc())
+			echo '<p>Não há partidas em andamento com apostas</p>';
+		else{
+			while ($resultado = $assoc->fetch_assoc()){
+			?>
+			<?php
+			$partida = $resultado['id_partida'];
+			$sql = " select * from partidas WHERE id_partida = $partida";
+			$res = mysqli_query($conexao, $sql);
+			$partida = mysqli_fetch_array($res);
+
+			$camp = $partida['id_camp'];
+			$sql = " select sigla from campeonatos WHERE id_camp = $camp";
+				$camp = mysqli_query($conexao, $sql);
+			$camp = mysqli_fetch_array($res);
+			?>
+			<h4><?=$camp['sigla'];?></h4>
+			<?php
+
+
+			#equipe 1
+			$equipe1 = $partida['id_equipe1'];
+			$sql = " select * from equipes WHERE id_equipe = $equipe1";
+			$equipe1 = mysqli_query($conexao, $sql);
+			$equipe1 = mysqli_fetch_array($equipe1);
+			#equipe 2
+			$equipe2 = $partida['id_equipe2'];
+			$sql = " select * from equipes WHERE id_equipe = $equipe2";
+			$equipe2 = mysqli_query($conexao, $sql);
+			$equipe2 = mysqli_fetch_array($equipe2);
+			
+			echo ''.$equipe1['sigla'].' VS '.$equipe2['sigla'].'<p> '.$partida['horario_termino'].'</p>';?>
+			</div>  <!--Div de partida-->
 		<?php
-
-
-		#equipe 1
-		$equipe1 = $partida['id_equipe1'];
-		$sql = " select * from equipes WHERE id_equipe = $equipe1";
-		$equipe1 = mysqli_query($conexao, $sql);
-		$equipe1 = mysqli_fetch_array($equipe1);
-		#equipe 2
-		$equipe2 = $partida['id_equipe2'];
-		$sql = " select * from equipes WHERE id_equipe = $equipe2";
-		$equipe2 = mysqli_query($conexao, $sql);
-		$equipe2 = mysqli_fetch_array($equipe2);
-		
-		echo ''.$equipe1['sigla'].' VS '.$equipe2['sigla'].'<p> '.$partida['horario_termino'].'</p>';?>
-        </div>  <!--Div de partida-->
-	<?php
-	}
-	?>
+		}
+	}?>
 	</div>
 </aside>
