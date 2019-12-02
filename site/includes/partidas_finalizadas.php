9a2cb86786aa92f9793753dafe5e50a48eb77a98
Marcelo EK <31110991+marceloek@users.noreply.github.com>
2019-12-01 21:46:08 -0300

    Update partidas_finalizadas.php

--- site/includes/partidas_finalizadas.php
+++ site/includes/partidas_finalizadas.php
@@ -1,30 +1,30 @@
-<?php
-	include "conexao.php";
+<aside class="partida_finalizadas">
+	<!-- container de partidas finalizadas -->
+	<div class="lista-partidas">
 
-	$consulta = "select id_equipe1, id_equipe2, pontos_equipe1, pontos_equipe2 from partidas where partidas.pontos_equipe1 and partidas.pontos_equipe2 is not null order by partidas.horario_termino desc";
-	$dados = mysqli_query($conexao, $consulta);
-	// calcula quantos dados retornaram
-	//$total = mysql_num_rows($dados);
-?>
-<div>
-	<h3>Ultimas partidas finalizadas:</h3>
-	<?php
-	if(!$dados->fetch_assoc())
-		echo '<p>Não há partidas finalizadas</p>';
-	else{
-    	while ($res = mysqli_fetch_array($dados)){
-    		$n1 = $res['id_equipe1'];
-			$n2 = $res['id_equipe2'];
-			
-    		$cnome1 = "select nome from equipes where id_equipe = $n1";
-			$nome1 = mysqli_query($conexao, $cnome1);
-			$nome1 = mysqli_fetch_array($nome1);
+		<h3>Ultimas partidas finalizadas:</h3>
+		<?php
+		$statement = $conexao->prepare("select id_equipe1, id_equipe2, pontos_equipe1, pontos_equipe2 from partidas where pontos_equipe1 and pontos_equipe2 is not null order by horario_termino desc limit 3");
+		$statement->execute();
+		$statement = $statement->get_result();
+		if(!$statement)
+			echo '<p>Não há partidas finalizadas</p>';
+		else{
+			while ($res = $statement->fetch_assoc()){
+				$n1 = $res['id_equipe1'];
+				$n2 = $res['id_equipe2'];
+				
+				$cnome1 = "select nome from equipes where id_equipe = $n1";
+				$nome1 = mysqli_query($conexao, $cnome1);
+				$nome1 = mysqli_fetch_array($nome1);
 
-			$cnome2 = "select nome from equipes where id_equipe = $n2";
-			$nome2 = mysqli_query($conexao, $cnome2);
-			$nome2 = mysqli_fetch_array($nome2);
+				$cnome2 = "select nome from equipes where id_equipe = $n2";
+				$nome2 = mysqli_query($conexao, $cnome2);
+				$nome2 = mysqli_fetch_array($nome2);
 
-			echo '<p><b>'.$nome1["nome"].'</b> '.$res['pontos_equipe1'].' VS '.$res['pontos_equipe2'].' <b>'.$nome2["nome"].'</b> </p>';
-		}
-	}?>
-</div>
+				echo '<p><b>'.$nome1["nome"].'</b> '.$res['pontos_equipe1'].' VS '.$res['pontos_equipe2'].' <b>'.$nome2["nome"].'</b> </p>';?>
+			<?php
+			}
+		}?>
+	</div>
+</aside>
